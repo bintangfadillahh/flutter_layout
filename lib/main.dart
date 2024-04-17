2518681617bea4f897d0278edfb100ad11287884
star2112 <bintangfadel73@gmail.com>
2024-04-18 04:13:53 +0700

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,371 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    const String appTitle = 'Trippy';
+    return MaterialApp(
+      title: appTitle,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(appTitle),
+        ),
+        // #docregion addWidget
+        body: const SingleChildScrollView(
+          child: Column(
+            children: [
+              ImageSection(
+                image: 'images/images.jpg',
+              ),
+              TitleSection(
+                name: 'Taman Siring Laut',
+                location: 'Pulau Laut Utara, Kab. Kotabaru, Kalimantan Selatan',
+              ),
+              ButtonSection(),
+              CustomText(
+                text: 'FACILITY',
+                padding: 16.0, // Set the padding
+                fontWeight: FontWeight.bold,
+              ),
+              AccessibilitySection(),
+              TextSection(
+                description:
+                    'Taman Siring Laut merupakan salah satu destinasi wisata yang wajib kamu kunjungi apabila sedang '
+                    'berkunjung ke Kotabaru. Lokasinya tidak jauh dari Kantor Bupati Kotabaru, sehingga mudah untuk kamu '
+                    'temui. Taman Siring Laut memiliki lapangan yang luas dengan ikon dua ikan todak kembar. Di tempat ini, '
+                    'kamu dapat bersantai menikmati indahnya pemandangan di sini hingga menikmati kuliner yang tersedia di '
+                    'sekitar Taman Siring Laut. ',
+              ),
+            ],
+          ),
+        ),
+        // #enddocregion addWidget
+      ),
+    );
+  }
+}
+
+class TitleSection extends StatelessWidget {
+  const TitleSection({
+    super.key,
+    required this.name,
+    required this.location,
+  });
+
+  final String name;
+  final String location;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: [
+          Expanded(
+            /*1*/
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                /*2*/
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: Text(
+                    name,
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Text(
+                  location,
+                  style: TextStyle(
+                    color: Colors.grey[500],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          /*3*/
+          // #docregion Icon
+          Icon(
+            Icons.star,
+            color: Colors.red[500],
+          ),
+          // #enddocregion Icon
+          const Text('69'),
+        ],
+      ),
+    );
+  }
+}
+
+class CustomText extends StatelessWidget {
+  final String text;
+  final double padding;
+  final FontWeight fontWeight;
+
+  const CustomText({
+    super.key,
+    required this.text,
+    this.padding = 0.0,
+    this.fontWeight = FontWeight.normal,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(padding),
+      child: Text(
+        text,
+        style: TextStyle(
+          fontWeight: fontWeight,
+        ),
+      ),
+    );
+  }
+}
+
+class ButtonSection extends StatelessWidget {
+  const ButtonSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final Color color = Theme.of(context).primaryColor;
+    return SizedBox(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          ButtonWithText(
+            color: color,
+            icon: Icons.near_me,
+            label: 'ROUTE',
+          ),
+          ButtonWithText(
+            color: color,
+            icon: Icons.share,
+            label: 'SHARE',
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ButtonWithText extends StatelessWidget {
+  const ButtonWithText({
+    super.key,
+    required this.color,
+    required this.icon,
+    required this.label,
+  });
+
+  final Color color;
+  final IconData icon;
+  final String label;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Padding(
+          padding: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class TextSection extends StatelessWidget {
+  const TextSection({
+    super.key,
+    required this.description,
+  });
+
+  final String description;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(32),
+      child: Text(
+        description,
+        softWrap: true,
+      ),
+    );
+  }
+}
+
+// #docregion AccessibilitySection
+
+class AccessibilitySection extends StatelessWidget {
+  const AccessibilitySection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final Color color = Theme.of(context).primaryColor;
+    return SizedBox(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          AccessibilityWithText(
+            color: color,
+            icon: Icons.local_parking,
+            label: 'PARKING AREA',
+          ),
+          AccessibilityWithText(
+            color: color,
+            icon: Icons.wc,
+            label: 'TOILET',
+          ),
+          AccessibilityWithText(
+            color: color,
+            icon: Icons.food_bank,
+            label: 'FOODCOURT',
+          ),
+          AccessibilityWithText(
+            color: color, 
+            icon: Icons.mosque, 
+            label: 'MUSHOLA',
+          ),
+           AccessibilityWithText(
+            color: color, 
+            icon: Icons.delete, 
+            label: 'TRASHCAN',
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class AccessibilityWithText extends StatelessWidget {
+  const AccessibilityWithText({
+    super.key,
+    required this.color,
+    required this.icon,
+    required this.label,
+  });
+
+  final Color color;
+  final IconData icon;
+  final String label;
+
+ @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Padding(
+          padding: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+// #docregion ImageSection
+class ImageSection extends StatelessWidget {
+  const ImageSection({super.key, required this.image});
+
+  final String image;
+
+  @override
+  Widget build(BuildContext context) {
+    // #docregion Image-asset
+    return Image.asset(
+      image,
+      width: 600,
+      height: 240,
+      fit: BoxFit.cover,
+    );
+    // #enddocregion Image-asset
+  }
+}
+// #enddocregion ImageSection
+
+// #docregion FavoriteWidget
+class FavoriteWidget extends StatefulWidget {
+  const FavoriteWidget({super.key});
+
+  @override
+  State<FavoriteWidget> createState() => _FavoriteWidgetState();
+}
+// #enddocregion FavoriteWidget
+
+// #docregion _FavoriteWidgetState, _FavoriteWidgetState-fields, _FavoriteWidgetState-build
+class _FavoriteWidgetState extends State<FavoriteWidget> {
+  // #enddocregion _FavoriteWidgetState-build
+  bool _isFavorited = true;
+  int _favoriteCount = 41;
+
+  // #enddocregion _FavoriteWidgetState-fields
+
+  // #docregion _toggleFavorite
+  void _toggleFavorite() {
+    setState(() {
+      if (_isFavorited) {
+        _favoriteCount -= 1;
+        _isFavorited = false;
+      } else {
+        _favoriteCount += 1;
+        _isFavorited = true;
+      }
+    });
+  }
+
+  // #enddocregion _toggleFavorite
+
+  // #docregion _FavoriteWidgetState-build
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Container(
+          padding: const EdgeInsets.all(0),
+          child: IconButton(
+            padding: const EdgeInsets.all(0),
+            alignment: Alignment.center,
+            icon: (_isFavorited
+                ? const Icon(Icons.star)
+                : const Icon(Icons.star_border)),
+            color: Colors.red[500],
+            onPressed: _toggleFavorite,
+          ),
+        ),
+        SizedBox(
+          width: 18,
+          child: SizedBox(
+            child: Text('$_favoriteCount'),
+          ),
+        ),
+      ],
+    );
+  }
+// #docregion _FavoriteWidgetState-fields
+}
+
